8fdc11d156d97eafc16d1d99800e4e06f51279c0
Jun <cygoh@umich.edu>
2018-01-23 08:35:20 -0800

    add media query for responsiveness

--- src/theme/grid.js
+++ src/theme/grid.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
+import { media } from './media';
 
 export const Container = styled.div`
-  padding-left: 50px;
-  padding-right: 50px;
+  padding-left: 60px;
+  padding-right: 60px;
   padding-top: 20px;
+  ${media.tablet`
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 15px;
+  `},
+  ${media.tablet`
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 15px;
+  `}
 `;
