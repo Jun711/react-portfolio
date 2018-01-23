f239a0cccb1c13bdf6eb0013601676066ca0aefb
Jun <cygoh@umich.edu>
2018-01-22 23:45:16 -0800

    add some global style

---
+++ src/theme/globalStyle.js
@@ -0,0 +1,11 @@
+import { injectGlobal } from 'styled-components';
+
+/* eslint-disabled */
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Indie+Flower');
+
+  body {
+    font-family: 'Indie Flower', cursive;
+    font-size: 24px;
+  }
+`
---
+++ src/theme/grid.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-top: 20px;
+`;
