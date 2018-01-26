b647039905dd629bf9deb01d8b4fbdfe69c8d1f0
Jun <cygoh@umich.edu>
2018-01-25 23:53:02 -0800

    to use FontAwesome

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes';
+import './index.css'
 import './theme/globalStyle'
 import registerServiceWorker from './registerServiceWorker';
+
+require('font-awesome-webpack');
 
 ReactDOM.render(routes, document.getElementById('root'));
 
