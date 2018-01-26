37b744334f68b701529d1cd967e0fede4af45934
Jun <cygoh@umich.edu>
2018-01-25 23:54:50 -0800

    Projects component

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ProjectContainer, ProjectItem, ProjectWrapper } from './Projects.style';
+
+class Projects extends Component {
+  render() {
+    return (
+        <ProjectContainer
+          row
+          justify={'flex-start'}
+          alignContent={'flex-start'}
+        >
+          <ProjectItem>123</ProjectItem>
+          <ProjectItem>456</ProjectItem>
+          <ProjectItem>abc</ProjectItem>
+          <ProjectItem>def</ProjectItem>
+          <ProjectItem>ghi</ProjectItem>
+        </ProjectContainer>
+    );
+  }
+}
+
+export default Projects;
---
+++ src/components/Projects/Projects.style.js
@@ -0,0 +1,63 @@
+import styled, { css } from 'styled-components';
+import { Flex, Div } from '../../theme/grid';
+import { media } from "../../theme/media";
+import { red, yellow, THEME_COLOR, white, blue } from '../../theme/variables';
+
+
+export const Title = styled.h1`
+  color: ${red};
+  font-weight: bold;
+  margin-top: 0;
+`;
+
+export const ProjectWrapper = styled(Div)`
+  background-color: ${blue};;
+  width: 100%; 
+`;
+
+export const ProjectContainer = styled(Flex)`
+  padding: 0;
+  margin: 0;
+  flex-wrap: wrap; 
+  background-color: ${red};
+  width: 1200px;
+  margin: 0 auto;   
+  
+  h3 {
+    text-align: center;
+    color: black;
+    
+    font-size: 2em;
+  }
+`;
+
+export const ProjectItem = styled(Div)`
+  padding: 5px;
+  height: 300px;
+  width: 300px;
+  margin: 10px;
+  background-color: ${yellow};
+  color: white;
+  font-weight: bold;
+  font-size: 3em;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 300px;
+  position: relative; 
+  display: block;   
+ 
+   &:hover {
+      &:after {
+        content: ' Overlay ';
+        cursor: pointer;
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        color: ${blue};
+        background-color: ${THEME_COLOR};
+        z-index: 2; 
+      }
+  }
+`;
