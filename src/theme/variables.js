c4bcd141d9ee002b1f729d17a0c4191e1e62bcae
Jun <cygoh@umich.edu>
2018-01-26 17:24:38 -0800

    add color variables

--- src/theme/variables.js
+++ src/theme/variables.js
@@ -2,9 +2,14 @@ export const yellow = '#fdee6c';
 export const red = '#f25f5c';
 export const blue = '#247ba0';
 export const black = '#424242';
+export const gray = '#828282';
+export const teal = '#42f4dc';
+export const green = '#41f480';
 export const white = '#fff';
 export const LOADER_COLOR = '#515c6d';
 export const THEME_COLOR = '#38414e';
 export const TEXT_COLOR = '#000';
-export const LIGHT_THEME_COLOR = '#98a3b3'
-export const DARK_THEME_COLOR = '#515c6d'
+export const LIGHT_THEME_COLOR = '#98a3b3';
+export const DARK_THEME_COLOR = '#515c6d';
+
+
