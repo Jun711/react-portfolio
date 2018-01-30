4d6703fef343ae7a69884661b94a5d5e6f14d558
Jun <cygoh@umich.edu>
2018-01-30 07:57:06 -0800

    remove react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './routes';
 import App from './containers/App/App';
 import './index.css'
 import './theme/globalStyle'
