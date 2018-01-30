17a04a0d0addabcce2e69394786690071e37ded0
Jun <cygoh@umich.edu>
2018-01-29 23:56:30 -0800

    style Copyright

--- src/components/Copyright/Copyright.style.js
+++ src/components/Copyright/Copyright.style.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 import { Div, Flex } from '../../theme/grid';
-import { black, DARK_THEME_COLOR } from '../../theme/variables';
+import { charcoal, white } from '../../theme/variables';
 
 export const Line = styled(Div)`
-  background-color: ${DARK_THEME_COLOR};
-  font-size: 0.7em;  
+  color: ${white};
+  font-size: 1em;
+  font-weight: 100;  
   margin: 0 auto;
 `;
 
@@ -12,8 +13,8 @@ export const Container = styled(Flex)`
   right: 0;
   left: 0;
   bottom: 0;
-  height: 1em;
-  background-color: ${black};
+  height: 1.5em;
+  background-color: ${charcoal};
   align-content: center;
   align-items: center;
 `;
