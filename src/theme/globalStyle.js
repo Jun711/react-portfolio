dced446a8707574e666aba0a86eb49b542cac35f
Jun <cygoh@umich.edu>
2018-01-29 23:58:58 -0800

    global style

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -1,29 +1,54 @@
 import { injectGlobal } from 'styled-components';
-import { THEME_COLOR } from "./variables";
+import { THEME_COLOR, charcoal } from "./variables";
 
 /* eslint-disabled */
 injectGlobal`
-  @import url('https://fonts.googleapis.com/css?family=Indie+Flower');
-
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+  
   body {
     margin: 0;
-    font-family: 'Indie Flower', cursive;
+    font-family: 'Open Sans', sans-serif;
     font-size: 24px;
+    font-color: ${charcoal};
+    height: 100%;
   }
   
   h1, h2, h3 {
     text-align: center;
-    margin-bottom: 0;
+    margin-bottom: 5;
+  }
+  
+  h1 {
+    font-weight: 700;
+  }
+  
+  h2 {
+    font-weight: 600;
+  }
+  
+  h3 {
+    font-weight: 500;
   }
   
   hr {
-    margin-top: 0;
+    margin: 25px auto;
     border: 2px solid ${THEME_COLOR};
     max-width: 14em;  
   }
   
   h3 + hr {
+    margin: 25px auto;
     border: 2px solid ${THEME_COLOR};
     max-width: 7em;  
+  }
+  
+  li {
+    letter-spacing: 0.0625em;
+    line-height: 1.5em;
+    font-size: 16px;
+  }
+  
+  .active {
+    text-decoration: underline;
   }
 `
--- src/theme/variables.js
+++ src/theme/variables.js
@@ -4,15 +4,13 @@ export const orange = '#f48c42';
 export const blue = '#247ba0';
 export const purple = '#6e41f4';
 export const pink = '#f441c7';
-export const black = '#424242';
+export const charcoal = '#424242';
 export const gray = '#828282';
+export const black = '#000';
 export const teal = '#42f4dc';
 export const green = '#41f480';
 export const white = '#fff';
-export const LOADER_COLOR = '#515c6d';
+export const grey = '#515c6d';
 export const THEME_COLOR = '#38414e';
-export const TEXT_COLOR = '#000';
 export const LIGHT_THEME_COLOR = '#98a3b3';
 export const DARK_THEME_COLOR = '#515c6d';
-
-
