cfbcb848fa8f52c1761eb45cfc43e321b8156c1e
Jun <cygoh@umich.edu>
2018-02-01 17:32:04 -0800

    update ProfileCard component

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
-import { Title } from './ProfileCard.style';
+import { ProfileContainer, ProfileImage, ProfileImageContainer } from './ProfileCard.style';
 import Network from '../SocialMedia/Network';
 
 class ProfileCard extends Component {
@@ -8,11 +8,24 @@ class ProfileCard extends Component {
   render() {
     return (
       <Container>
-        <Title>
-          <h1>Jun Goh</h1>
-          <h3>Software Developer</h3>
-          <Network/>
-        </Title>
+        <ProfileContainer
+          column
+          justify={'f'}
+          alignItems={'center'}
+        >
+          {/*<h1>Jun Goh</h1>*/}
+          {/*<h3>Software Developer</h3>*/}
+          <ProfileImageContainer
+            row
+            justify={'center'}
+          >
+            <ProfileImage
+              alt="Jun\'s photo"
+              src={require('../../assets/img/rpc-cover.jpg')}
+            />
+          </ProfileImageContainer>
+          <Network doBounce={true}/>
+        </ProfileContainer>
       </Container>
     )
   }
--- src/components/ProfileCard/ProfileCard.style.js
+++ src/components/ProfileCard/ProfileCard.style.js
@@ -1,46 +1,28 @@
-import styled from 'styled-components'
-import { media } from '../../theme/media'
-import { green, white } from '../../theme/variables';
+import styled from 'styled-components';
+import { FixWidthFlexContainer, Flex } from '../../theme/grid';
+import { green } from '../../theme/colors';
+import { bounce } from '../../theme/doBounce';
 
 export const Image = styled.img`
   width: 100%;
 `;
 
-export const Title = styled.div`
-  height: 13em;
-  font-size: 2em;
-  ${media.larger`
-    height: 12em;
-    font-size: 1.75em;  
-  `}
-  ${media.desktop`
-    height: 12em;
-    font-size: 1.75em;  
-  `}
-  ${media.tablet`
-    height: 11em;
-    font-size: 1.5em;  
-  `}
-  ${media.phone`
-    height: 11em;
-    font-size: 1em;  
-  `}
-  
+export const ProfileContainer = styled(Flex)`
+  height: 8em;
+  font-size: 1.8em;
   background-color: ${green};
   background-size: cover;
   background-position: center;
-  
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  
-  color: ${white};
+`;
 
-  h1 {
-    margin: 0;
-  }
-  h3 {
-    margin-top: 0;
-  }
+export const ProfileImageContainer = styled(FixWidthFlexContainer)`
+    animation: ${bounce} 1.4s ease-out 0.7s;
+`;
+
+export const ProfileImage = styled.img`
+  display: block;
+  border-radius: 100%;
+  height: 170px;
+  width: 170px;
+  margin: 0.7em;
 `;
