f270260128802894b819e4ae0fab31f4b5feb6cb
Jun <cygoh@umich.edu>
2018-02-02 00:50:57 -0800

    update Project overlay

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,24 +1,65 @@
-import React, { Component } from "react";
-// import styled, { css, keyframes, ThemeProvider } from "styled-components";
-// import { A } from "theme/types";
-// import media from "theme/media";
-// import { connect } from "react-redux";
-// import { push } from "react-router-redux";
-// import { open as openMenu, close as closeMenu } from "redux/modules/menu";
-// import { Flex, Div, Relative } from "theme/grid";
-// import SocialMedia from "components/SocialMedia/SocialMedia";
-// import { easeInOutQuart, easeOutQuart } from "theme/easeFunctions";
-import { Container, Content } from './Project.style'
-
+import React, { Component } from 'react';
+import {
+  CloseButtonContainer, Content, DescContainer, DescGrid, DescItem, FixWidthProjContainer, InfoSet,
+  ProjectContainer, ProjectDetailContainer, ProjectImage, ProjectURL
+} from './Project.style';
+import FontAwButton from '../FontAwButton/FontAwButton';
 
 class Project extends Component {
-  render() {
+
+  renderProjectDesc(project, center) {
+    let justifyContent = center ? 'center' : 'flex-start';
+    const projDes = project.description || [];
+    const descLines = projDes.map((line, index) =>
+      <DescItem key={index}>{line}</DescItem>
+    )
     return (
-      <Container>
+      <DescContainer
+        column
+        justify={justifyContent}
+      >
+        <DescGrid>
+          {descLines}
+          {project.repoUrl &&
+          <DescItem key={-1}>
+            <ProjectURL
+              target='_blank'
+              href={project.repoUrl}>Check out the <span>GitHub repo<i className='fa fa-github' aria-hidden='true'></i></span>
+            </ProjectURL>
+          </DescItem>}
+        </DescGrid>
+      </DescContainer>
+    );
+  }
+
+  render() {
+    const title = this.props.project.title || 'Project'
+    const projectSwitch = this.props.projectSwitch;
+    const image = this.props.project.imageUrl || 'default-proj-img.jpg';
+    return (
+      <ProjectContainer
+        hide={!projectSwitch}
+      >
         <Content>
-          ABC
+          <CloseButtonContainer onClick={() => this.props.closeProject(false)}>
+            <FontAwButton
+              class={'fa fa-times'}/>
+          </CloseButtonContainer>
+          <FixWidthProjContainer>
+            <h2>{title}</h2>
+            <ProjectDetailContainer
+              row
+              justify={'center'}
+              alignItems={'flex-start'}
+            >
+              {this.props.project.imageUrl &&
+              <InfoSet><ProjectImage alt={title} src={require(`../../assets/img/${image}`)}/></InfoSet>
+              }
+              <InfoSet>{this.renderProjectDesc(this.props.project, this.props.project.imageUrl)}</InfoSet>
+            </ProjectDetailContainer>
+          </FixWidthProjContainer>
         </Content>
-      </Container>
+      </ProjectContainer>
     );
   }
 }
--- src/components/Project/Project.style.js
+++ src/components/Project/Project.style.js
@@ -1,150 +1,130 @@
 import styled, { css } from 'styled-components';
-import { Div, Flex, Relative } from '../../theme/grid';
-import { blue, white, teal, green, purple } from '../../theme/variables';
-import { Link } from 'react-scroll';
+import { Div, FixWidthFlexContainer, FixWidthRelContainer, Flex } from '../../theme/grid';
+import { UnderlineA } from '../../theme/animatedTags';
+import { blue, charcoal, orange, themeColor, white, yellow } from '../../theme/colors';
 import { media } from '../../theme/media';
 
-// const Container = styled(Flex)`
-//   color: ${menuTextColor};
-//
-//   position: fixed;
-//   width: 100vw;
-//   height: 100vh;
-//   top: 0;
-//   left: 0;
-//   font-size: 16px;
-//
-//   justify-content: center;
-//   flex-direction: column;
-//
-//   background-color: ${menuBackground};
-//   z-index: 98;
-//
-//   text-align: center;
-//
-//   // p {
-//   //   color: white;
-//   // }
-//
-//   h1 {
-//     margin: 0;
-//     margin-top: 25px;
-//   }
-//
-//   ${media.tablet`
-//     h1 {
-//       font-size: 1.5em;
-//     }
-//   `}
-//   transition: transform 0.6s ${easeInOutQuart};
-//   ${({ hide }) => hide && css`
-//     transform: translateY(-100%);
-//   `}
-// `;
-
-export const Content = styled(Relative)`
-  background-color: ${green};
-  padding-top: 20px;
-  padding-left: 100px;
-  padding-right: 100px;
-  ${media.tablet`
-    padding-top: 0;
-    padding-left: 40px;
-    padding-right: 40px;
-  `}
+export const ButtonFlex = styled(Flex)`
+  height: 4em;
+  width: 4em;
+  background-color: ${blue};
+  color: ${orange};
+   border-radius: 50%;
 `;
 
-// const Background = styled.div`
-//   position: fixed;
-//   background-image: url(${require("assets/body-background.png")});
-//   background-size: 100%;
-//   background-position: center center;
-//   top: 0;
-//   left: 0;
-//   width: 100vw;
-//   height: 100vh;
-//   z-index: 100;
-//   opacity: 0.1;
-//   pointer-events: none;
-//   transition: opacity .4s;
-// `;
-//
-// const revealUpKeyframes = keyframes`
-//   from { transform: translateY(105%); }
-//   to { transform: translateY(0); }
-// `;
-//
-// const RevealUp = styled(Div)`
-//   overflow: hidden;
-//   & > * {
-//     ${({ show }) => show && css`
-//       transform: translateY(105%);
-//       animation: ${revealUpKeyframes} .6s ${easeOutQuart};
-//       animation-delay: 0.6s;
-//       animation-fill-mode: forwards;
-//     `}
-//   }
-// `;
-export const Container = styled(Flex)`
-  color: ${blue};
-  top: 7vh;
-  background-color: ${teal};
+export const ProjectContainer = styled.div`
+  background-color: ${white};
   position: fixed;
   width: 100vw;
   height: 100vh;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
   opacity: 1;
-  justify-content: center;
-  flex-direction: column;
   z-index: 100;
-  text-align: center;
-  h1 {
-    margin: 0;
-    margin-top: 25px;
-  }
-
-  transition: transform 0.7s ease-in-out, opacity 1s linear;
+  text-align: center; 
+  transition: transform 0.7s ease-in-out, opacity 1s ease-in-out;
   
   ${({hide}) => hide && css`
     opacity: 0;
-    transform: translateY(-100%);
+    transform: translateX(100%);
   `}
 `;
 
-export const ComponentContainer = styled(Div)`
-  background-color: ${teal};
-  width: 100%; 
+export const Content = styled(FixWidthRelContainer)``;
+
+export const FixWidthProjContainer = styled(FixWidthRelContainer)`
+  background-color: ${white};
+  overflow: scroll;
+  height: 100vh;
+  padding-top: 80px;
+  ${media.tablet`
+    padding-top: 55px;
+  `}
 `;
 
-
-export const NavItem = styled(Link)`
-  margin-right: 20px;
-  
-  font-size: 1.3em;
-  cursor: pointer;
-  color: ${white};  
-  
-  position: relative;
-  line-height: 1.3em;
-  text-decoration: none;
+export const CloseButtonContainer = styled.div`
+  position: absolute;
+  z-index: 101;
+  top: 60px;
+  right: 60px;
  
-  &:hover {
-    color: ${blue};
-    &:before {
-      left: 1px;
-      opacity: 1;
-    }
-    &:after {
-      content: ' ';
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 100%;
-      background-color: ${white};
-      z-index: -1; 
-      transform: scale(1.2, 1.7);
-    }
+  ${media.tablet`
+    top: 5px;
+    right: 5px;
+  `}
+`;
+
+export const ProjectDetailContainer = styled(FixWidthFlexContainer)`
+  padding: 0;
+  flex-wrap: wrap; 
+  h3 {
+    text-align: center;
+    margin: 0;
+    font-size: 1em;
   }
 `;
+
+export const InfoSet = styled(Div)`
+  height: 700px;
+  width: 450px;
+  ${media.tablet`
+    width: 300px;
+  `}
+  ${media.tablet`
+    width: 450px;
+  `}
+  font-weight: bold;
+  font-size: 1em;
+  text-align: center;
+  vertical-align: middle;
+  position: relative; 
+  display: block;   
+  
+  h3 {
+    // color: ${charcoal};
+  }
+`;
+
+export const ProjectImage = styled.img`
+  border: 1px solid ${themeColor};
+  height: 600px;
+  width: 330px;
+  // ${media.larger`
+  //   height: 450px;
+  //   width: 250px;
+  // `}
+  // ${media.desktop`
+  //   height: 500px;
+  //   width: 220px;
+  // `}
+  // ${media.tablet`
+  //   height: 320px;
+  //   width: 170px;
+  // `}
+  // ${media.phone`
+  //   height: 500px;
+  //   width: 300px;
+  // `}
+`;
+
+export const DescContainer = styled(Flex)`
+  height: 600px;
+  padding: 0;
+  margin: 0;
+  ${media.tablet`
+    justify-content: flex-start;
+  `}
+`
+
+export const ProjectURL = styled(UnderlineA)`
+  font-size: 20px
+`;
+
+export const DescItem = styled.li`
+  font-size: 20px;
+  text-align: left;
+`;
+
+export const DescGrid = styled.ul``;
